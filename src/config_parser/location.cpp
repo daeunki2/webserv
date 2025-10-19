a83752ca084d4e8e4a598ca04b9c214f1a21f1c1
Daeun Kim <daeunki2@f2r7s1.clusters.42paris.fr>
2025-10-19 18:27:49 +0200

    location calss update

--- src/config_parser/location.cpp
+++ src/config_parser/location.cpp
@@ -6,7 +6,7 @@
 /*   By: daeunki2 <daeunki2@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/19 15:56:12 by daeunki2          #+#    #+#             */
-/*   Updated: 2025/10/19 17:46:40 by daeunki2         ###   ########.fr       */
+/*   Updated: 2025/10/19 18:27:21 by daeunki2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,22 +30,74 @@ location& location::operator=(const location& src)
 {
 	if (this != &src)
 	{
-		
+		destination = src.destination;
+		path = src.path;
+		index = src.index;
+		root= src.root;
+		upload_store = src.upload_store;
+		cgi_ext =cgi_ext;
+		cgi_pass = cgi_pass;
+		auto_user_file = src.auto_user_file;
+		auto_basic = src.auto_basic;
+		autoindex = src.autoindex;
+		upload_enable = src.upload_enable;
+		m_get = src.m_get; 
+		m_post = src.m_post;
+		m_delete = src.m_delete;
 	}
-	return (*this)
-};	
+	return (*this);
+};
+
+
+
+
+//getters
 std::string location::get_destination()
 {
 	return(destination);
-}
+};
 std::string location::get_path()
 {
 	return(path);	
-}
-int	location::get_index()
+};
+
+std::string location::get_root()
+{
+	return(root);
+};
+std::string location::get_upload_store()
+{
+	return(upload_store);
+};
+std::string location::get_cgi_ext()
+{
+	return(cgi_ext);
+};
+std::string location::get_cgi_pass()
+{
+	return(cgi_pass);
+};
+std::string location::get_auto_user_file()
+{
+	return(auto_user_file);
+};
+
+std::vector<int>	location::get_index()
 {
 	return(index);
 }
+
+bool	location::get_on_or_off(std::string flag)
+{
+	if(flag == "auto_basic")
+		return (auto_basic);
+	else if (flag == "autoindex")
+		return(autoindex);
+	else
+		return(upload_enable);
+}
+
+
 bool location::get_method(std::string flag)
 {
 	if(flag == "get")
@@ -54,21 +106,55 @@ bool location::get_method(std::string flag)
 		return(m_delete);
 	else
 		return(m_post);
-}
+};
 
+
+
+//setters
 void location::set_destination(std::string input)
 {
 	destination = input;
-}
+};
 void location::set_path(std::string input)
 {
 	path = input;
-}
-void location::set_index(int input)
+};
+
+void	location::set_root(std::string input)
+{
+	root = input;
+};
+void	location::set_upload_store(std::string input)
+{
+	upload_store = input;
+};
+void	location::set_cgi_ext(std::string input)
+{
+	cgi_ext = input;
+};
+void	location::set_cgi_pass(std::string input)
+{
+	cgi_pass = input;
+};
+void	location::set_auto_user_file(std::string input)
+{
+	auto_user_file = input;
+};
+void	location::set_index(std::vector<int> input)
 {
 	index = input;
-}
-void location::set_method(std::string flag, bool status)
+};
+void	location::set_on_or_off(std::string flag, bool status)
+{
+	if(flag == "auto_basic")
+		auto_basic = status;
+	else if (flag == "autoindex")
+		autoindex= status;
+	else
+		upload_enable = status;
+};
+
+void	location::set_method(std::string flag, bool status)
 {
 	if(flag == "get")
 		m_get = status;
@@ -76,4 +162,4 @@ void location::set_method(std::string flag, bool status)
 		m_delete = status;
 	else
 		m_post = status;
-}
+};
--- src/config_parser/location.hpp
+++ src/config_parser/location.hpp
@@ -6,7 +6,7 @@
 /*   By: daeunki2 <daeunki2@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/19 15:56:05 by daeunki2          #+#    #+#             */
-/*   Updated: 2025/10/19 17:45:19 by daeunki2         ###   ########.fr       */
+/*   Updated: 2025/10/19 18:25:27 by daeunki2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,18 +15,27 @@
 
 #include <iostream>
 #include <string>
+#include <vector>
 /*
 ps. if you don't like the name, you cna change! 
 */
 class location
 {
 	private : 
-	std::string destination;
+	std::string destination;// location /destination {
 	std::string path;
-	int index; 
-	bool m_get;
-	bool m_post;
-	bool m_delete;
+	std::vector<int> index; // fd of index files
+	std::string root;
+	std::string upload_store;
+	std::string cgi_ext;
+	std::string cgi_pass;
+	std::string auto_user_file;
+	bool auto_basic;
+	bool autoindex;
+	bool upload_enable;
+	bool m_get; // allow_methood
+	bool m_post; // allow_methood
+	bool m_delete; // allow_methood
 
 	public :
 	location();
@@ -35,16 +44,28 @@ class location
 	location& operator=(const location& src);
 	
 	//getters
-	std::string get_destination();
-	std::string get_path();
-	int			get_index();
-	bool		get_method(std::string flag);
+	std::string 		get_destination();
+	std::string 		get_path();
+	std::string 		get_root();
+	std::string 		get_upload_store();
+	std::string 		get_cgi_ext();
+	std::string 		get_cgi_pass();
+	std::string 		get_auto_user_file();
+	std::vector<int> 	get_index();
+	bool				get_on_or_off(std::string flag);
+	bool				get_method(std::string flag);
 
 	//setters
-	void		set_destination(std::string input);
-	void		set_path(std::string input);
-	void		set_index(int input);
-	void		set_method(std::string flag, bool status);
+	void			set_destination(std::string input);
+	void			set_path(std::string input);
+	void			set_root(std::string input);
+	void			set_upload_store(std::string input);
+	void			set_cgi_ext(std::string input);
+	void			set_cgi_pass(std::string input);
+	void			set_auto_user_file(std::string input);
+	void			set_index(std::vector<int> input);
+	void			set_method(std::string flag, bool status);
+	void			set_on_or_off(std::string flag, bool status);
 };
 
 #endif
